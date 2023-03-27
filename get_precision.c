70e31bfbfcb1bcd2c2cb166216a2febf65807215
Mary-Afrika <marygahar@gmail.com>
2023-03-27 04:56:19 -0700

    get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -14,19 +14,22 @@ int get_precision(const char *format, int *i, va_list list)
 int curr_i = *i + 1;
 int precision = -1;
 
-if (format[curr_i] != '.')
+if
+(format[curr_i] != '.')
 return (precision);
 
 precision = 0;
 
 for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 {
-if (is_digit(format[curr_i]))
+if
+(is_digit(format[curr_i]))
 {
 precision *= 10;
 precision += format[curr_i] - '0';
 }
-else if (format[curr_i] == '*')
+else if
+(format[curr_i] == '*')
 {
 curr_i++;
 precision = va_arg(list, int);
@@ -39,4 +42,5 @@ break;
 *i = curr_i - 1;
 
 return (precision);
+
 }
